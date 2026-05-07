20d44599530ca887e356cdda5c42a7e869c8d13b
bryannwanyanwu <bryannwanyanwu@gmail.com>
2026-05-07 09:10:48 -0700

    Java Program

---
+++ Hello.java
@@ -0,0 +1,12 @@
+ public class Hello{
+	 public static void main (String[] args){
+		 System.out.println("Hello everyone, I am learning Java");
+		 System.out.print("oh wow, learning Java is fun. ");
+		 System.out.print("Hurray, I am Happy. ");
+		 System.out.printf("java was created in %dn", 1992);
+		 System.out.printf("Java was made free for %c%.2f ",'$',0.0000);
+		 System.out.printf("Do you love Java%c, %b",'?', true);
+		 System.out.printf( "yes learning %s is fun","Java");
+	 }
+	
+}
